f48464cf08fbb2bfa109aa07f27b64bbf90a2be3
Matthew Ingle <mingle1@radar.gsw.edu>
2016-02-09 17:11:58 -0500

    +Added basic mailer with responsive layout for mobile viewing.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,23 @@
 class UserMailer < ActionMailer::Base
 	 helper :application # gives access to all helpers defined within `application_helper`.
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
-	 default from: "gsw.gameification@gmail.com"
+	 default from: "Gameification <gsw.gameification@gmail.com>"
   layout 'mailer'
+  include Devise::Mailers::Helpers
+
+def confirmation_instructions(record, token, opts={})
+  @token = token
+  devise_mail(record, :confirmation_instructions, opts)
+end
+
+def reset_password_instructions(record, token, opts={})
+  @token = token
+  devise_mail(record, :reset_password_instructions, opts)
+end
+
+def unlock_instructions(record, token, opts={})
+  @token = token
+  devise_mail(record, :unlock_instructions, opts)
+end
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,16 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+config.action_mailer.default_url_options = { :host => 'localhost' }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => "gmail.com",
+      :user_name            => "gsw.gameification@gmail.com",
+      :password             => "$uperu$er12",
+      :authentication       => :login,
+      :enable_starttls_auto => true
+}
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
