570286ae8f83514bffb0c742169e0119cdf35789
Matthew Ingle <mingle1@radar.gsw.edu>
2016-02-09 16:51:41 -0500

    +Testing Mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,6 @@
 class UserMailer < ActionMailer::Base
+	 helper :application # gives access to all helpers defined within `application_helper`.
+  include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
 	 default from: "gsw.gameification@gmail.com"
   layout 'mailer'
 end
