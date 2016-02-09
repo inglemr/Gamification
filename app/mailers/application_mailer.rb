bbe5c4b7c5fe438e58d01315ae109cf46adc18e6
Matthew Ingle <mingle1@radar.gsw.edu>
2016-02-09 14:29:41 -0500

    +Added action mailer +Styled forgot your password page

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "gsw.gameification@gmail.com"
+  layout 'mailer'
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,17 @@
 Rails.application.configure do
+
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => "gmail.com",
+      :user_name            => "gsw.gameification@gmail.com",
+      :password             => "$uperu$er12",
+      :authentication       => :login,
+      :enable_starttls_auto => true
+}
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
