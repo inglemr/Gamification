f9e8a324d7f5aaf4d04612e5803ebefe68a9e16f
Matthew Ingle <mingle1@radar.gsw.edu>
2016-02-15 16:22:29 -0500

    +Fix for location edit path

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,4 @@
 class Room < ActiveRecord::Base
-	validates :room_number, :presence => true, :uniqueness => {:scope => :location_id}
+	validates :room_number, :presence => true, :uniqueness => {:scope => :location_id, message: "Room number already exists for this location"}
 	belongs_to :locations
 end
