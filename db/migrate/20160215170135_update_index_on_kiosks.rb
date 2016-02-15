b2322cda6b3f518a82d4ca88a91dbedb8977757e
Matthew Ingle <mingle1@radar.gsw.edu>
2016-02-15 12:50:41 -0500

    +Fix for migration

--- db/migrate/20160215170135_update_index_on_kiosks.rb
+++ db/migrate/20160215170135_update_index_on_kiosks.rb
@@ -1,7 +1,6 @@
 class UpdateIndexOnKiosks < ActiveRecord::Migration
 	def up
 	  sql = 'DROP INDEX index_kiosks_on_email'
-	  sql << ' ON kiosks' if Rails.env == 'production' # Heroku pg
 	  ActiveRecord::Base.connection.execute(sql)
 	end
 end
