b34f841792ed10dd37c0d2173875829bca173cb9
Matthew Ingle <mingle1@radar.gsw.edu>
2016-02-18 17:16:56 -0500

    +Added data source to calendar

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,5 +8,22 @@ class DashboardController < ApplicationController
     	format.json { render json: EventsDatatable.new(view_context) }
   	end
   end
-  
+
+  def cal_events
+  	@events = Event.where('end_time >= ?', DateTime.now).upcoming_first#.past_events
+  	array = Array.new
+
+  	@events.each do |event|
+  		output = Hash.new
+  		output[:title] = event.event_name
+  		output[:start] = event.day_time.to_s(:calendar)
+  		output[:end] = event.end_time.to_s(:calendar)
+  		output[:url] = url_for(:controller => 'events',:action => 'show', :id => event.id)
+  		array << output
+  	end
+	  respond_to do |format|
+	    format.json { render :json => array }
+	  end
+  end
+    
 end
--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,1 +1,2 @@
 Time::DATE_FORMATS[:datepicker] = "%m/%d/%Y %I:%M %p"
+Time::DATE_FORMATS[:calendar] = "%Y-%m-%d %I:%M %p"
