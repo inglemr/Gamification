acab84868228313afbe22332639d6cad18132f41
Matthew Ingle <mingle1@radar.gsw.edu>
2016-02-18 19:19:53 -0500

    +Updated calendar to show events with better styling

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -14,11 +14,18 @@ class DashboardController < ApplicationController
   	array = Array.new
 
   	@events.each do |event|
+  		color = "%06x" % (event.id * 0xffffff)
   		output = Hash.new
   		output[:title] = event.event_name
   		output[:start] = event.day_time.to_s(:calendar)
   		output[:end] = event.end_time.to_s(:calendar)
   		output[:url] = url_for(:controller => 'events',:action => 'show', :id => event.id)
+  		output[:color] = color
+  		output[:textColor] = "white"
+  		classArray = Array.new 
+  		classArray << "event"
+      classArray << "bg-color-greenLight"
+  		output[:className] = classArray
   		array << output
   	end
 	  respond_to do |format|
