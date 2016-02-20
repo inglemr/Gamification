3a3fad4fd7ef283a653b210f204471f90602b583
Matthew Ingle <mingle1@radar.gsw.edu>
2016-02-20 15:05:25 -0500

    +Only show created events in menu if user can create events +Add time format for kiosk +UI tweaks for swipe page for an event

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,3 +1,3 @@
 Time::DATE_FORMATS[:datepicker] = "%m/%d/%Y %I:%M %p"
 Time::DATE_FORMATS[:calendar] = "%Y-%m-%d %H:%M"
-Time::DATE_FORMATS[:datebase] = "%Y-%m-%d %H:%M %p"
+Time::DATE_FORMATS[:kiosk] = ""
