e9963890860228e5593806034e52e3a091387566
Matthew Ingle <mingle1@radar.gsw.edu>
2016-02-27 21:33:41 -0500

    +Added lockable to accounts. Account is locked after 20 failed attempts

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,10 +11,10 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-config.scoped_views = true
+  config.scoped_views = true
   # Configure the class responsible to send e-mails.
   config.mailer = 'UserMailer'
-config.secret_key = 'ee2fde88c4a9dcbd6c49e7d1cafb15321f6a62729bce6fab95abc860792164dc48b19fc9d3989ccdfdbe8ec631223ab8c35702fe311f171d6bfd5cde14d612b6'
+  config.secret_key = 'ee2fde88c4a9dcbd6c49e7d1cafb15321f6a62729bce6fab95abc860792164dc48b19fc9d3989ccdfdbe8ec631223ab8c35702fe311f171d6bfd5cde14d612b6'
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
@@ -159,27 +159,27 @@ config.secret_key = 'ee2fde88c4a9dcbd6c49e7d1cafb15321f6a62729bce6fab95abc860792
   # Defines which strategy will be used to lock an account.
   # :failed_attempts = Locks an account after a number of failed attempts to sign in.
   # :none            = No lock strategy. You should handle locking by yourself.
-  # config.lock_strategy = :failed_attempts
+  config.lock_strategy = :failed_attempts
 
   # Defines which key will be used when locking and unlocking an account
-  # config.unlock_keys = [ :email ]
+  config.unlock_keys = [ :email ]
 
   # Defines which strategy will be used to unlock an account.
   # :email = Sends an unlock link to the user email
   # :time  = Re-enables login after a certain amount of time (see :unlock_in below)
   # :both  = Enables both strategies
   # :none  = No unlock strategy. You should handle unlocking by yourself.
-  # config.unlock_strategy = :both
+  config.unlock_strategy = :email
 
   # Number of authentication tries before locking an account if lock_strategy
   # is failed attempts.
-  # config.maximum_attempts = 20
+  config.maximum_attempts = 20
 
   # Time interval to unlock the account if :time is enabled as unlock_strategy.
   # config.unlock_in = 1.hour
 
   # Warn on the last attempt before the account is locked.
-  # config.last_attempt_warning = true
+  config.last_attempt_warning = true
 
   # ==> Configuration for :recoverable
   #
