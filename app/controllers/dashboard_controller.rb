7f1d33d9fcba6038bd3a69057aa3c4afab18f75b
Matthew Ingle <mingle1@radar.gsw.edu>
2016-03-15 12:50:51 -0400

    +Added ability for admin to see all leaderboards

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,10 @@ class DashboardController < ApplicationController
   before_filter :load_permissions
   def index
     @userLeader = User.where(:class_type => current_user.class_type).order("points DESC").limit(10)
+    @freshmen = User.where(:class_type => "Freshmen").order("points DESC").limit(10)
+    @sophmore = User.where(:class_type => "Sophmore").order("points DESC").limit(10)
+    @junior = User.where(:class_type => "Junior").order("points DESC").limit(10)
+    @senior = User.where(:class_type => "Senior").order("points DESC").limit(10)
   	respond_to do |format|
     	format.html
     	format.json { render json: EventsDatatable.new(view_context) }
