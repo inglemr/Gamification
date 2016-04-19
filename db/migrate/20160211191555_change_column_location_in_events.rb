5bdfd996246088ca79a2bddd4287b49eeef1b96e
Matthew Ingle <mingle1@radar.gsw.edu>
2016-04-18 20:34:51 -0400

    fix

--- db/migrate/20160211191555_change_column_location_in_events.rb
+++ db/migrate/20160211191555_change_column_location_in_events.rb
@@ -1,6 +1,5 @@
 class ChangeColumnLocationInEvents < ActiveRecord::Migration
-  def change
-  	add_column(:events, :location_id, :integer)
+  def change]
   	add_index :events, :location_id
   end
 end
