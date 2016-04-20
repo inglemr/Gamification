ed0c9010838cd63d8b784d1bc618a4e05bf94faf
Matthew Ingle <mingle1@radar.gsw.edu>
2016-04-20 03:36:04 -0400

    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,13 +3,13 @@ Rails.application.configure do
 config.action_mailer.default_url_options = { :host => 'gsw-capstone.heroku.com' }
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
+    :address   => "smtp.sendgrid.net",
+    :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => ENV["SENDGRID_USERNAME"],
+    :password  => ENV["SENDGRID_PASSWORD"], # SMTP password is any valid API key, when user_name is "apikey".
+    :authentication => 'login',
+    :domain => 'gsw-capstone.heroku.com'
 }
 
   # Settings specified here will take precedence over those in config/application.rb.
